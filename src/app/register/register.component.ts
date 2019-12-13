546c8ae1b0ff13c0f27de231f4c29113a7338d5e
FloTheFlower <florentina.hofbauer@gmail.com>
2019-12-13 13:22:28 -0800

    hi

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,12 +8,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  form = {
-      firstName: "",
-      secondName: "",
-      password: "",
-      email: ""
+form = {
+  firstName: "",
+  secondName: "",
+  password: "",
+  email: ""
 }
   
   constructor(private _userService: UserService,
