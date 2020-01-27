a3f2cf31ae638a565962b475cb20e7c21d634dc1
FloTheFlower <florentina.hofbauer@gmail.com>
2020-01-26 19:46:51 -0800

    j

---
+++ src/app/authguard.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { UserService } from './user.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthguardService implements CanActivate {
+
+  constructor( public router: Router, public _userService: UserService) { }
+
+
+canActivate(): boolean {
+  if (!this._userService.loggedIn) {
+    this.router.navigate(['login']);
+    prompt("Please login or register")
+    return false;
+  }
+  return true;
+}
+}
